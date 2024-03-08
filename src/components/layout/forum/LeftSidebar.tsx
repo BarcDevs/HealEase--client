a7ab84bb3d07073971196d7f83f26818f8728565
Bar Cohen <sscohen99@gmail.com>
2024-03-08 04:09:51 +0200

    added left sidebar placeholder

---
+++ src/components/layout/forum/LeftSidebar.tsx
@@ -0,0 +1,10 @@
+const LeftSidebar = ({}) => (
+    <aside className={'w-64 min-h-screen border-r border-gray-200 p-4 overflow-y-auto sticky top-0 left-0 flex_col'}>
+        <p>Category 1</p>
+        <p>Category 2</p>
+        <p>Category 3</p>
+    </aside>
+)
+
+
+export default LeftSidebar
--- src/pages/layouts/ForumLayout.tsx
+++ src/pages/layouts/ForumLayout.tsx
@@ -1,9 +1,10 @@
 import {Outlet} from '@tanstack/react-router'
+import LeftSidebar from '@/components/layout/forum/LeftSidebar.tsx'
 
 const ForumLayout = ({}) => (
     <>
         {/*<Header/>*/}
-        {/*<LeftSidebar/>*/}
+        <LeftSidebar/>
         <Outlet/>
         {/*<RightSidebar/>*/}
     </>
