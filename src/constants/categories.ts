4855dc939d8d119c6d37350bc021bf08e7c470c5
Bar Cohen <sscohen99@gmail.com>
2024-03-08 05:35:51 +0200

    added categories constant

---
+++ src/constants/categories.ts
@@ -0,0 +1,79 @@
+export default {
+    en: {
+        injuryTypes: [
+            'Fractures',
+            'Spinal Cord Injuries',
+            'Traumatic Brain Injuries',
+            'Musculoskeletal Injuries',
+            'Internal Injuries',
+            'Burn Injuries',
+            'Other Specific Injuries'
+        ],
+        rehabilitation: [
+            'Physical Therapy',
+            'Occupational Therapy',
+            'Speech Therapy',
+            'Mobility Aids and Devices',
+            'Assistive Technologies',
+            'Rehabilitation Exercises',
+            'Recovery Milestones'
+        ],
+        mentalHealth: [
+            'Coping Strategies',
+            'Emotional Support',
+            'Stress Management',
+            'Anxiety and Depression',
+            'Mindfulness and Meditation',
+            'Therapeutic Techniques',
+            'Self-Care Practices'
+        ],
+        peerSupport: [
+            'Introduction and Community Guidelines',
+            'Success Stories',
+            'Personal Experiences',
+            'Q&A and Advice',
+            'Support Groups',
+            'Motivational Posts',
+            'General Discussion'
+        ]
+    },
+    he: {
+        'סוגי פגיעה': [
+            'שברים',
+            'פגיעות בחוט השדרה',
+            'פגיעות במוח טראומטיות',
+            'פגיעות במערכת השיירה והשלד',
+            'פגיעות פנימיות',
+            'פציעות כיבים',
+            'פציעות ספציפיות נוספות'
+        ],
+        'התאוששות': [
+            'פיזיותרפיה',
+            'טיפול בעיסוק',
+            'טיפול בדיבור',
+            'ציוד נייד ומכשירים',
+            'טכנולוגיות סיוע',
+            'תרגילי התאוששות',
+            'שלבי התאוששות'
+        ],
+        'בריאות נפשית': [
+            'אסטרטגיות ניהול מצב רוח',
+            'תמיכה רגשית',
+            'ניהול מתח',
+            'חרדה ודיכאון',
+            'מיינדפולנס ומדיטציה',
+            'טכניקות טיפוליות',
+            'פרקטיקות פיזור רוח'
+        ],
+        'תמיכה מעמדית': [
+            'הקדמה והנחיות קהילתיות',
+            'סיפורי הצלחה',
+            'חוויות אישיות',
+            'שאלות ותשובות וייעוץ',
+            'קבוצות תמיכה',
+            'פוסטים מחזקים',
+            'דיון כללי'
+        ]
+    }
+}
+
