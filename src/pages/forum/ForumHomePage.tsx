34894cf079cd93a0a0f289299e1583e8fe26d68c
Bar Cohen <sscohen99@gmail.com>
2024-03-12 21:38:35 +0200

    added loader to forum homepage

--- src/pages/forum/ForumHomePage.tsx
+++ src/pages/forum/ForumHomePage.tsx
@@ -1,13 +1,17 @@
 import Page from '@/components/shared/ui/Page.tsx'
-import {posts} from '@/mocks/post.ts'
 import PostList from '@/components/forum/PostList.tsx'
+import {useLoaderData} from '@tanstack/react-router'
 
-const ForumHomePage = ({}) => (
-    <Page>
-        {/*<FilterButtons />*/}
-        <PostList posts={posts}/>
-    </Page>
-)
+const ForumHomePage = ({}) => {
+    const posts = useLoaderData({from: '/_forum/forum/'})
+
+    return (
+        <Page>
+            {/*<FilterButtons />*/}
+            <PostList posts={posts}/>
+        </Page>
+    )
+}
 
 
 export default ForumHomePage
--- src/routes/_forum/forum/index.tsx
+++ src/routes/_forum/forum/index.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ForumHomePage from '@/pages/forum/ForumHomePage.tsx'
+import {fetchPosts} from '@/mocks/loaders.ts'
 
 export const Route = createFileRoute('/_forum/forum/')({
+  loader: async () => {
+    return fetchPosts()
+  },
   component: ForumHomePage
 })
