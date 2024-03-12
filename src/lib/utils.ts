911045812ef06c78da9fdec8a8c58a0818ecf0c8
Bar Cohen <sscohen99@gmail.com>
2024-03-12 19:52:29 +0200

    added toRelative function to modify a date object to relative time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
+import {formatDistance} from 'date-fns'
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const toRelative = (date: Date) => {
+  return formatDistance(date, new Date(), {
+    addSuffix: true
+  })
 }
