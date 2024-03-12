ea255ff6e4cbd4b2a52c236b13ae9589d5ad58f8
Bar Cohen <sscohen99@gmail.com>
2024-03-12 19:57:56 +0200

    added toShortNumber utility function to add a suffix to a long number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,21 @@ export const toRelative = (date: Date) => {
     addSuffix: true
   })
 }
+
+/**
+ * Converts a number to a short string representation with K, M, or B suffix.
+ *
+ * @param {number} num - the number to be converted
+ * @return {string} the short string representation of the number
+ */
+export const toShortNumber = (num: number): string => {
+  if (num >= 1000000000) {
+    return `${(num / 1000000000).toFixed(1)}B`
+  } else if (num >= 1000000) {
+    return `${(num / 1000000).toFixed(1)}M`
+  } else if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}K`
+  } else {
+    return num.toString()
+  }
+}
