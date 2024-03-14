a9835b8d57337e5b3a93560e2c8ceb0c5fa17788
Bar Cohen <sscohen99@gmail.com>
2024-03-14 23:46:35 +0200

    forum: add forum page headline

--- src/pages/forum/ForumHomePage.tsx
+++ src/pages/forum/ForumHomePage.tsx
@@ -2,6 +2,7 @@ import Page from '@/components/shared/ui/Page.tsx'
 import PostList from '@/components/forum/PostList.tsx'
 import {getRouteApi} from '@tanstack/react-router'
 import FilterButtons from '@/components/forum/FilterButtons.tsx'
+import Headline from '@/components/forum/Headline.tsx'
 
 const route = getRouteApi('/_forum/forum/')
 
@@ -10,6 +11,7 @@ const ForumHomePage = ({}) => {
 
     return (
         <Page>
+            <Headline title={'All Posts'} createPost/>
             <FilterButtons/>
             <PostList posts={posts}/>
         </Page>
