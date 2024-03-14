5db3e878b46316148f8b59004e95357a6ff94cdc
Bar Cohen <sscohen99@gmail.com>
2024-03-14 21:34:03 +0200

    PostList: list surround with section tag

--- src/components/forum/PostList.tsx
+++ src/components/forum/PostList.tsx
@@ -2,13 +2,15 @@ import {Post} from '@/types/forum'
 import PostCard from '@/components/forum/PostCard.tsx'
 
 const PostList = ({posts} : {posts: Post[]}) => (
-    <ul>
-        {posts.map((post) => (
-            <li key={post._id} className={'flex--col gap-5 p-3'}>
-                <PostCard post={post}/>
-            </li>
-        ))}
-    </ul>
+    <section>
+        <ul>
+            {posts.map((post) => (
+                <li key={post._id} className={'flex--col gap-5 p-3'}>
+                    <PostCard post={post}/>
+                </li>
+            ))}
+        </ul>
+    </section>
 )
 
 
