0a46a026b7b852f3219d6fda7c4f98966a265ec7
Bar Cohen <sscohen99@gmail.com>
2024-03-18 01:56:13 +0200

    lang: replace old language handling with new

--- src/pages/layouts/RootLayout.tsx
+++ src/pages/layouts/RootLayout.tsx
@@ -1,28 +1,17 @@
 import {Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/router-devtools'
 import Header from '@/components/layout/root/Header.tsx'
-import {useState} from 'react'
 
-const RootLayout = ({}) => {
-    const [dir, setDir] = useState('ltr')
-
-    const toggleDir = () => setDir(dir === 'ltr' ? 'rtl' : 'ltr')
-
-    return (
-        <>
-            <html lang="en" dir={dir}>
-            <body>
-            <Header toggleDir={toggleDir}/>
-            <div className={'relative flex'}>
-                <Outlet/>
-            </div>
-            {/*<Footer/>*/}
-            <TanStackRouterDevtools/>
-            </body>
-            </html>
-        </>
-    )
-}
+const RootLayout = ({}) => (
+    <>
+        <Header/>
+        <div className={'relative flex'}>
+            <Outlet/>
+        </div>
+        {/*<Footer/>*/}
+        <TanStackRouterDevtools/>
+    </>
+)
 
 
 export default RootLayout
