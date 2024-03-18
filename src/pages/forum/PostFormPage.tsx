1949f679797a9059718dcef2b578d1cef89f66a8
Bar Cohen <sscohen99@gmail.com>
2024-03-18 02:40:05 +0200

    feat[create-post]: add component for /create route

---
+++ src/pages/forum/PostFormPage.tsx
@@ -0,0 +1,11 @@
+import Page from '@/components/shared/ui/Page.tsx'
+import PostForm from '@/components/forum/PostForm.tsx'
+
+const PostFormPage = ({type}: { type: 'create' | 'edit' }) => (
+    <Page>
+        <PostForm type={type}/>
+    </Page>
+)
+
+
+export default PostFormPage
