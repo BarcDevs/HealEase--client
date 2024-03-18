8ce494c8a3f7977680401bfd3a282a499a8d98d2
Bar Cohen <sscohen99@gmail.com>
2024-03-18 02:45:20 +0200

    feat[create-post]: add form zod schema

---
+++ src/validations/forms/postSchema.ts
@@ -0,0 +1,40 @@
+import * as z from 'zod'
+import categories from '@/data/forum/categories.ts'
+import config from '@/config/schema/postForm.ts'
+
+export const postSchema = z.object({
+    category: z.string(),
+    title: z.string()
+        .min(1, 'Title is required')
+        .min(config.title.minLength, 'Title is too short')
+        .max(config.title.maxLength, 'Title is too long'),
+    content: z.string()
+        .min(1, 'Content is required')
+        .min(config.content.minLength, 'Content is too short')
+        .max(config.content.maxLength,
+            `Content must be less than ${config.content.maxLength} characters`),
+    tags: z.array(
+        z.string()
+            .min(config.tags.minLength, 'Tag is too short')
+            .max(config.tags.maxLength, 'Tag is too long')
+            .toUpperCase()
+    )
+        .min(1, 'At least one tag is required')
+        .max(config.tags.max,
+            `Cannot have more than ${config.tags.max} tags`)
+})
+    .superRefine(({category}, ctx) => {
+        if (!category) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: 'Category is required'
+            })
+        } else if (!categories.find(cat => cat.key === category)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: 'Category is invalid'
+            })
+        }
+    })
+
+export type PostSchema = z.infer<typeof postSchema>
