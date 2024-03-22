f4d4cac9912d3092245cbf999f57ca472919130e
Bar Cohen <sscohen99@gmail.com>
2024-03-22 15:57:20 +0200

    [feat] added links to sidebar

--- src/components/layout/forum/LeftSidebar.tsx
+++ src/components/layout/forum/LeftSidebar.tsx
@@ -1,5 +1,6 @@
 import {groupedCategories} from '@/data/forum/categories.ts'
 import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '@/components/ui/accordion.tsx'
+import {Link} from '@tanstack/react-router'
 
 const LeftSidebar = ({}) => {
     const categoryGroups = Object.keys(groupedCategories)
@@ -9,22 +10,26 @@ const LeftSidebar = ({}) => {
             className={'flex--col sticky top-0 min-h-screen w-64 overflow-y-auto border-gray-200 p-4 ltr:left-0 ltr:border-r rtl:right-0 rtl:border-l'}>
             <nav className={'flex--col gap-4'}>
                 <Accordion type={'single'} collapsible>
-                {categoryGroups.map(group => (
-                    <AccordionItem key={group} value={group}>
-                        <AccordionTrigger className={'font-bold'}>
-                            {group}
-                        </AccordionTrigger>
-                        {
-                            groupedCategories[group]?.map(category => (
-                                <AccordionContent
-                                    key={category.name}
-                                >
-                                    {category.name}
-                                </AccordionContent>
-                            ))
-                        }
-                    </AccordionItem>
-                ))}
+                    {categoryGroups.map(group => (
+                        <AccordionItem key={group} value={group}>
+                            <AccordionTrigger className={'font-bold hover:no-underline'}>
+                                {group}
+                            </AccordionTrigger>
+                            {
+                                groupedCategories[group]?.map(category => (
+                                    <AccordionContent
+                                        key={category.key}
+                                    >
+                                        <Link to={'/forum'}
+                                              search={{category: category.key}}
+                                        >
+                                            {category.name}
+                                        </Link>
+                                    </AccordionContent>
+                                ))
+                            }
+                        </AccordionItem>
+                    ))}
                 </Accordion>
             </nav>
         </aside>
