e323c982d1b421b1b655e57c5c6794186e8bf8f2
Bar Cohen <sscohen99@gmail.com>
2024-03-22 17:58:14 +0200

    [feat] added all post button to categories

--- src/components/layout/forum/LeftSidebar.tsx
+++ src/components/layout/forum/LeftSidebar.tsx
@@ -12,6 +12,12 @@ const LeftSidebar = ({}) => {
             className={'flex--col sticky top-0 min-h-screen w-64 overflow-y-auto border-gray-200 p-4 ltr:left-0 ltr:border-r rtl:right-0 rtl:border-l'}>
             <nav className={'flex--col gap-4'}>
                 <Accordion type={'single'} collapsible>
+                    <AccordionItem value={'all'}>
+                        <Link to={'/forum'}
+                              className={'flex flex-1 items-center justify-between font-bold py-4'}>
+                            <p>All Posts</p>
+                        </Link>
+                    </AccordionItem>
                     {categoryGroups.map(group => (
                         <AccordionItem key={group} value={group}>
                             <AccordionTrigger className={'font-bold hover:no-underline'}>
