b63c4a86ac37d2e82a96dc76dc7587abfdd06246
Bar Cohen <sscohen99@gmail.com>
2024-03-24 22:34:42 +0200

    [rfc] added filter function to mock loader

--- src/mocks/loaders.ts
+++ src/mocks/loaders.ts
@@ -1,13 +1,27 @@
 import {posts} from '@/mocks/post.ts'
 import {ForumSearchParams} from '@/types/router'
 import {Post} from '@/types/forum'
+import {Filter} from '@/data/forum/filters.ts'
 
-export const fetchPosts = async ({category}: ForumSearchParams): Promise<Post[]> => {
+export const fetchPosts = async ({filter = Filter.newest, category}: ForumSearchParams): Promise<Post[]> => {
     if (category) {
-        return posts.filter(post => post.category === category)
+        return sortBy(posts, filter).filter(post => post.category === category)
     }
 
-    return posts
+    return sortBy(posts, filter)
 }
 
 export const fetchPost = async (postId: string) => posts.find(post => post._id === postId)
+
+const sortBy = (posts : Post[], sort: Filter) => {
+    switch (sort) {
+        case Filter.newest:
+            return posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        case Filter.popular:
+            return posts.sort((a, b) => b.votes.positive - b.votes.negative - (a.votes.positive - a.votes.negative))
+        case Filter.hot:
+            return posts.sort((a, b) => b.replies.length - a.replies.length)
+        case Filter.unanswered:
+            return posts.filter(post => post.replies.length === 0)
+    }
+}
