d49dda72fff73931421b1c98df8cff2a8d40db53
Bar Cohen <sscohen99@gmail.com>
2024-03-25 00:31:05 +0200

    [rfc] fixed filter buttons overriding the entire search query

--- src/components/forum/FilterButtons.tsx
+++ src/components/forum/FilterButtons.tsx
@@ -5,7 +5,9 @@ import {Badge} from '@/components/ui/badge.tsx'
 const FilterButtons = ({}) => (
     <section className={'flex-row-start gap-3 p-3'}>
         {Object.values(Filter).map(filter => (
-            <Link key={filter} to={'/forum'} search={{filter}} className={'flex-center'}>
+            <Link key={filter} to={'/forum'}
+                  search={search => ({...search, filter})}
+                  className={'flex-center'}>
                 <Badge className={'rounded-md border-none px-4 py-2 capitalize'}>
                     {filter}
                 </Badge>
