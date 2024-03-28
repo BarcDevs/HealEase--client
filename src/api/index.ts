b6a345dfd94504e8d6d7c963975d66e31f501918
Bar Cohen <sscohen99@gmail.com>
2024-03-28 21:01:51 +0200

    [feat] added api version to axios settings

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,6 @@ import axios from 'axios'
 import config from '@/config'
 
 export const api = axios.create({
-    baseURL: `${config.serverUrl}/api`,
+    baseURL: `${config.serverUrl}/api/${config.serverApiVersion}`,
+    withCredentials: true
 })
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 type Config = {
     serverUrl: string
     hostname: string
+    serverApiVersion: string
     replaysSessionSampleRate: number
     tinyMceApiKey: string
 }
@@ -10,6 +11,7 @@ const env = import.meta.env
 const config: Config = {
     serverUrl: env.VITE_SERVER_URL,
     hostname: env.VITE_HOSTNAME,
+    serverApiVersion: env.VITE_SERVER_API_VERSION || 'v1',
     replaysSessionSampleRate: Number(env.VITE_SENTRY_REPLAYS_SESSION_SAMPLE_RATE),
     tinyMceApiKey: env.VITE_TINY_MCE_API_KEY
 }
