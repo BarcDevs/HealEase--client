8a437467639f5e0d660bebee9c973ac762550414
Bar Cohen <sscohen99@gmail.com>
2024-03-28 22:06:17 +0200

    [feat] added function to retrieve the csrf token from store

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -4,6 +4,9 @@ import {removeTokenAction, setTokenAction} from '@/store/tokenSlice.ts'
 import {getMe} from '@/api/auth.ts'
 import {loginAction, logoutAction} from '@/store/authSlice.ts'
 
+export const getCsrfToken = () =>
+    store.getState().token.token
+
 export const handleSuccessLogin = (data: LoginResponse) => {
     store.dispatch(setTokenAction(data.token))
     getMe().then(userData =>
