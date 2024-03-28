31598b20ec00ffd3e02696b6d2ac8c237bf9c287
Bar Cohen <sscohen99@gmail.com>
2024-03-28 20:57:31 +0200

    [rfc] sentry init moved to separate file

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
-import {useEffect, StrictMode} from 'react'
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import {createRouter, RouterProvider} from '@tanstack/react-router'
-import * as Sentry from '@sentry/react'
 import './styles/index.css'
-
-// Import the generated route tree
 import {routeTree} from './routeTree.gen'
 import ContextProvider from '@/context'
-import config from '@/config'
+import {sentryInit} from '@/utils/sentry.ts'
 
 // Create a new router instance
 const router = createRouter({routeTree})
@@ -20,27 +17,7 @@ declare module '@tanstack/react-router' {
 }
 
 // Sentry Configuration
-Sentry.init({
-    dsn: 'https://94ccf213e00340996348fa63f2605456@o4506954726703104.ingest.us.sentry.io/4506954741055488',
-    integrations: [
-        Sentry.browserTracingIntegration(),
-        Sentry.metrics.metricsAggregatorIntegration(),
-        // @ts-expect-error
-        Sentry.reactRouterV6BrowserTracingIntegration({
-            useEffect
-        }),
-        Sentry.replayIntegration({
-            maskAllText: false,
-            blockAllMedia: false
-        })
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0,
-    tracePropagationTargets: [config.hostname, config.serverUrl + '/api/*'],
-    // Session Replay
-    replaysSessionSampleRate: config.replaysSessionSampleRate,
-    replaysOnErrorSampleRate: 1.0
-})
+sentryInit()
 
 // Render the app
 const rootElement = document.getElementById('app')!
