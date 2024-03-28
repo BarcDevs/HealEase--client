77f9b5876a102aec0dbcb2403fe623a348913f98
Bar Cohen <sscohen99@gmail.com>
2024-03-29 00:34:01 +0200

    [rfc] api now being called from handlers instead of the contrary

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,27 +1,27 @@
 import {api} from '@/api/index.ts'
-import {LoginResponse} from '@/types/responses/auth.ts'
-import {handleLogout, handleSuccessLogin} from '@/handlers/auth.ts'
-import {SignupSchema} from '@/validations/forms/signupSchema.ts'
 import {User} from '@/types'
+import {Response} from '@/types/responses'
+import {LoginResponse} from '@/types/responses/auth.ts'
+import {SignupSchema} from '@/validations/forms/signupSchema.ts'
+import {LoginSchema} from '@/validations/forms/loginSchema.ts'
 
-export const login = async (email: string, password: string) => {
+export const login = async (credentials: LoginSchema) => {
     const {data} =
-        await api.get<LoginResponse>('/auth/login', {
-            data: {email, password}
+        await api.get<Response<LoginResponse>>('/auth/login', {
+            data: credentials
         })
 
-    handleSuccessLogin(data)
-    return !!data
+    return data
 }
 
-export const signup = async (userData: SignupSchema) => {
-    const {data} = await api.post<User>('/auth/signup', userData)
-    return !!data
+export const signup = async (userData: Omit<SignupSchema, 'confirmPassword'>) => {
+    const {data} =
+        await api.post<Response<User>>('/auth/signup', userData)
+    return data
 }
 
 export const logout = async () => {
     await api.get('/auth/logout')
-    handleLogout()
 }
 
 export const getMe = async () =>
--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -1,19 +1,24 @@
-import {LoginResponse} from '@/types/responses/auth.ts'
 import store from '@/store'
 import {removeTokenAction, setTokenAction} from '@/store/tokenSlice.ts'
-import {getMe} from '@/api/auth.ts'
+import {getMe, login, logout} from '@/api/auth.ts'
 import {loginAction, logoutAction} from '@/store/authSlice.ts'
+import {LoginSchema} from '@/validations/forms/loginSchema.ts'
 
 export const getCsrfToken = () =>
     store.getState().token.token
 
-export const handleSuccessLogin = (data: LoginResponse) => {
+export const handleLogin = async (credentials: LoginSchema) => {
+    const {data} = await login(credentials)
+    if (!data) return
+
     store.dispatch(setTokenAction(data.token))
     getMe().then(userData =>
         store.dispatch(loginAction(userData)))
 }
 
-export const handleLogout = () => {
+export const handleLogout = async () => {
+    await logout()
+
     store.dispatch(removeTokenAction())
     store.dispatch(logoutAction())
 }
