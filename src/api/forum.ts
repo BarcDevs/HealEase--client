35eaff31ceeab48d0d4bb9d4e3f9094822db0c66
Bar Cohen <sscohen99@gmail.com>
2024-03-28 23:00:54 +0200

    [feat] added ApiResponse to represent the data returned from api actions

--- src/api/forum.ts
+++ src/api/forum.ts
@@ -1,42 +1,41 @@
 import {api} from '@/api/index.ts'
 import {Post, Reply} from '@/types/forum.ts'
-import {ForumSearchParams} from '@/types/router.ts'
 import {PostSchema} from '@/validations/forms/postSchema.ts'
 import {getCsrfToken} from '@/handlers/auth.ts'
-import {AxiosPromise} from 'axios'
+import {ApiResponse, Response} from '@/types/responses'
 
-export const fetchPosts = async (query: ForumSearchParams): AxiosPromise<Post[]> =>
-    api.get<Post[]>('/forum/posts', {data: query})
+export const fetchPosts = async (query: any): ApiResponse<Post[]> =>
+    await api.get<Response<Post[]>>('/forum/posts', {data: query})
 
-export const fetchPost = async (postId: string) : AxiosPromise<Post> =>
-    api.get<Post>(`/forum/posts/${postId}`)
+export const fetchPost = async (postId: string): ApiResponse<Post> =>
+    api.get<Response<Post>>(`/forum/posts/${postId}`)
 
-export const createPost = async (post: PostSchema) : AxiosPromise<Post> =>
-    api.post<Post>('/forum/posts', {
+export const createPost = async (post: PostSchema): ApiResponse<Post> =>
+    api.post<Response<Post>>('/forum/posts', {
         csrfToken: getCsrfToken(),
         post
     })
 
-export const updatePost = async (postId: string, post: PostSchema) : AxiosPromise<Post> =>
-    api.put<Post>(`/forum/posts/${postId}`, {
+export const updatePost = async (postId: string, post: PostSchema): ApiResponse<Post> =>
+    api.put<Response<Post>>(`/forum/posts/${postId}`, {
         csrfToken: getCsrfToken(),
         post
     })
 
-export const deletePost = async (postId: string) =>
-    api.delete(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfToken()}})
+export const deletePost = async (postId: string): ApiResponse<{}> =>
+    api.delete<Response<{}>>(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfToken()}})
 
-export const fetchReplies = async (postId: string) : AxiosPromise<Reply[]> =>
-    api.get<Reply[]>(`/forum/posts/${postId}/replies`)
+export const fetchReplies = async (postId: string): ApiResponse<Reply[]> =>
+    api.get<Response<Reply[]>>(`/forum/posts/${postId}/replies`)
 
-export const createReply = async (postId: string, reply: Reply) : AxiosPromise<Reply> =>
-    api.post<Reply>(`/forum/posts/${postId}/replies`, {
+export const createReply = async (postId: string, reply: Reply): ApiResponse<Reply> =>
+    api.post<Response<Reply>>(`/forum/posts/${postId}/replies`, {
         csrfToken: getCsrfToken(),
         reply
     })
 
-export const updateReply = async (postId: string, replyId: string, reply: Reply) : AxiosPromise<Reply> =>
-    api.put<Reply>(`/forum/posts/${postId}/replies/${replyId}`, {
+export const updateReply = async (postId: string, replyId: string, reply: Reply): ApiResponse<Reply> =>
+    api.put<Response<Reply>>(`/forum/posts/${postId}/replies/${replyId}`, {
         csrfToken: getCsrfToken(),
         reply
     })
