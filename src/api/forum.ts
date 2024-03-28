a03865de1b2e1debac221b7ce86e5345dc7421c4
Bar Cohen <sscohen99@gmail.com>
2024-03-28 22:07:18 +0200

    [feat] added crud operations for posts and comments

--- src/api/forum.ts
+++ src/api/forum.ts
@@ -1,6 +1,43 @@
 import {api} from '@/api/index.ts'
-import {Post} from '@/types/forum.ts'
+import {Post, Reply} from '@/types/forum.ts'
+import {ForumSearchParams} from '@/types/router.ts'
+import {PostSchema} from '@/validations/forms/postSchema.ts'
+import {getCsrfToken} from '@/handlers/auth.ts'
 
-export const fetchPosts = async () => api.get<Post[]>('/forum/posts')
+export const fetchPosts = async (query: ForumSearchParams) =>
+    api.get<Post[]>('/forum/posts', {data: query})
 
-export const fetchPost = async (postId: string) => api.get<Post>(`/forum/posts/${postId}`)
+export const fetchPost = async (postId: string) =>
+    api.get<Post>(`/forum/posts/${postId}`)
+
+export const createPost = async (post: PostSchema) =>
+    api.post<Post>('/forum/posts', {
+        csrfToken: getCsrfToken(),
+        post
+    })
+
+export const updatePost = async (postId: string, post: PostSchema) =>
+    api.put<Post>(`/forum/posts/${postId}`, {
+        csrfToken: getCsrfToken(),
+        post
+    })
+
+export const deletePost = async (postId: string) =>
+    api.delete(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfToken()}})
+
+export const fetchReplies = async (postId: string) =>
+    api.get<Reply[]>(`/forum/posts/${postId}/replies`)
+
+export const createReply = async (postId: string, reply: Reply) =>
+    api.post<Reply>(`/forum/posts/${postId}/replies`, {
+        csrfToken: getCsrfToken(),
+        reply
+    })
+
+export const updateReply = async (postId: string, replyId: string, reply: Reply) =>
+    api.put<Reply>(`/forum/posts/${postId}/replies/${replyId}`, {
+        csrfToken: getCsrfToken(),
+        reply
+    })
+
+
