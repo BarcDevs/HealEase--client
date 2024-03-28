54fc8d9cb9d2ec507d78b2855bacddd4e9e15932
Bar Cohen <sscohen99@gmail.com>
2024-03-28 22:21:42 +0200

    [rfc] added return types to api actions

--- src/api/forum.ts
+++ src/api/forum.ts
@@ -3,20 +3,21 @@ import {Post, Reply} from '@/types/forum.ts'
 import {ForumSearchParams} from '@/types/router.ts'
 import {PostSchema} from '@/validations/forms/postSchema.ts'
 import {getCsrfToken} from '@/handlers/auth.ts'
+import {AxiosPromise} from 'axios'
 
-export const fetchPosts = async (query: ForumSearchParams) =>
+export const fetchPosts = async (query: ForumSearchParams): AxiosPromise<Post[]> =>
     api.get<Post[]>('/forum/posts', {data: query})
 
-export const fetchPost = async (postId: string) =>
+export const fetchPost = async (postId: string) : AxiosPromise<Post> =>
     api.get<Post>(`/forum/posts/${postId}`)
 
-export const createPost = async (post: PostSchema) =>
+export const createPost = async (post: PostSchema) : AxiosPromise<Post> =>
     api.post<Post>('/forum/posts', {
         csrfToken: getCsrfToken(),
         post
     })
 
-export const updatePost = async (postId: string, post: PostSchema) =>
+export const updatePost = async (postId: string, post: PostSchema) : AxiosPromise<Post> =>
     api.put<Post>(`/forum/posts/${postId}`, {
         csrfToken: getCsrfToken(),
         post
@@ -25,16 +26,16 @@ export const updatePost = async (postId: string, post: PostSchema) =>
 export const deletePost = async (postId: string) =>
     api.delete(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfToken()}})
 
-export const fetchReplies = async (postId: string) =>
+export const fetchReplies = async (postId: string) : AxiosPromise<Reply[]> =>
     api.get<Reply[]>(`/forum/posts/${postId}/replies`)
 
-export const createReply = async (postId: string, reply: Reply) =>
+export const createReply = async (postId: string, reply: Reply) : AxiosPromise<Reply> =>
     api.post<Reply>(`/forum/posts/${postId}/replies`, {
         csrfToken: getCsrfToken(),
         reply
     })
 
-export const updateReply = async (postId: string, replyId: string, reply: Reply) =>
+export const updateReply = async (postId: string, replyId: string, reply: Reply) : AxiosPromise<Reply> =>
     api.put<Reply>(`/forum/posts/${postId}/replies/${replyId}`, {
         csrfToken: getCsrfToken(),
         reply
