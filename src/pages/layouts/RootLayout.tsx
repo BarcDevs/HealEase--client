76295e4f7c58884440fe451b5f2e053f2e42af1a
Bar Cohen <sscohen99@gmail.com>
2024-03-29 15:59:59 +0300

    [feat] added useLocalStorage to root component

--- src/pages/layouts/RootLayout.tsx
+++ src/pages/layouts/RootLayout.tsx
@@ -2,18 +2,23 @@ import * as Sentry from '@sentry/react'
 import {Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/router-devtools'
 import Header from '@/components/layout/root/Header.tsx'
+import {useLocalStorage} from '@/hooks/useLocalStorage.ts'
 
 const RootLayout = Sentry.withProfiler(
-    ({}) => (
-        <>
-            <Header/>
-            <div className={'relative flex'}>
-                <Outlet/>
-            </div>
-            {/*<Footer/>*/}
-            <TanStackRouterDevtools/>
-        </>
-    ))
+    ({}) => {
+        useLocalStorage()
+
+        return (
+            <>
+                <Header/>
+                <div className={'relative flex'}>
+                    <Outlet/>
+                </div>
+                {/*<Footer/>*/}
+                <TanStackRouterDevtools/>
+            </>
+        )
+    })
 
 
 export default RootLayout
