d586c3f27dc5800e37ab98b5c9f8f61b76e03ec6
Bar Cohen <sscohen99@gmail.com>
2024-03-29 17:11:50 +0300

    [feat] added validation before load on create and edit post

--- src/routes/_forum/forum/posts/$postId_.edit.tsx
+++ src/routes/_forum/forum/posts/$postId_.edit.tsx
@@ -1,8 +1,10 @@
 import {createFileRoute} from '@tanstack/react-router'
 import PostFormPage from '@/pages/forum/PostFormPage.tsx'
 import {postLoader} from '@/handlers/loaders/forum.ts'
+import {validateUser} from '@/handlers/loaders/auth.ts'
 
 export const Route = createFileRoute('/_forum/forum/posts/$postId/edit')({
+    beforeLoad: validateUser,
     loader: async ({params}) => {
         return postLoader(params.postId) // todo pass post data through route context
     },
--- src/routes/_forum/forum/posts/create.tsx
+++ src/routes/_forum/forum/posts/create.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import PostFormPage from '@/pages/forum/PostFormPage.tsx'
+import {validateUser} from '@/handlers/loaders/auth.ts'
 
 export const Route = createFileRoute('/_forum/forum/posts/create')({
+  beforeLoad: validateUser,
   component: () => <PostFormPage type={'create'}/>,
 })
