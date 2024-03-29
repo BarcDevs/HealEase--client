e67408bb5eb598d672ad78a88bbb28c880ff0d98
Bar Cohen <sscohen99@gmail.com>
2024-03-29 13:54:07 +0300

    [fix] fixed data being transferred as an unstructured object

--- src/api/forum.ts
+++ src/api/forum.ts
@@ -1,43 +1,43 @@
 import {api} from '@/api/index.ts'
 import {Post, Reply} from '@/types/forum.ts'
 import {PostSchema} from '@/validations/forms/postSchema.ts'
-import {getCsrfToken} from '@/handlers/auth.ts'
+import {getCsrfTokenFromStore} from '@/handlers/auth.ts'
 import {ApiResponse, Response} from '@/types/responses'
 
 export const fetchPosts = async (query: any): ApiResponse<Post[]> =>
-    await api.get<Response<Post[]>>('/forum/posts', {data: query})
+    await api.get<Response<Post[]>>('/forum/posts', {params: query})
 
 export const fetchPost = async (postId: string): ApiResponse<Post> =>
     api.get<Response<Post>>(`/forum/posts/${postId}`)
 
 export const createPost = async (post: PostSchema): ApiResponse<Post> =>
     api.post<Response<Post>>('/forum/posts', {
-        csrfToken: getCsrfToken(),
-        post
+        csrfToken: getCsrfTokenFromStore(),
+        ...post
     })
 
 export const updatePost = async (postId: string, post: PostSchema): ApiResponse<Post> =>
     api.put<Response<Post>>(`/forum/posts/${postId}`, {
-        csrfToken: getCsrfToken(),
-        post
+        csrfToken: getCsrfTokenFromStore(),
+        ...post
     })
 
-export const deletePost = async (postId: string): ApiResponse<{}> =>
-    api.delete<Response<{}>>(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfToken()}})
+export const deletePost = async (postId: string): ApiResponse<object> =>
+    api.delete<Response<object>>(`/forum/posts/${postId}`, {data: {csrfToken: getCsrfTokenFromStore()}})
 
 export const fetchReplies = async (postId: string): ApiResponse<Reply[]> =>
     api.get<Response<Reply[]>>(`/forum/posts/${postId}/replies`)
 
 export const createReply = async (postId: string, reply: Reply): ApiResponse<Reply> =>
     api.post<Response<Reply>>(`/forum/posts/${postId}/replies`, {
-        csrfToken: getCsrfToken(),
-        reply
+        csrfToken: getCsrfTokenFromStore(),
+        ...reply
     })
 
 export const updateReply = async (postId: string, replyId: string, reply: Reply): ApiResponse<Reply> =>
     api.put<Response<Reply>>(`/forum/posts/${postId}/replies/${replyId}`, {
-        csrfToken: getCsrfToken(),
-        reply
+        csrfToken: getCsrfTokenFromStore(),
+        ...reply
     })
 
 
