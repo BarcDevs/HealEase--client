d06a0ee2a5606058742b7c1c4a6b5a072caef975
Bar Cohen <sscohen99@gmail.com>
2024-06-30 06:07:41 +0300

    fix(prod): temporary remove sentry until errors are fixed

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import {RouterProvider} from '@tanstack/react-router'
 import './styles/index.css'
 import ContextProvider from '@/context'
-import {sentryInit} from '@/utils/sentry'
 import {router} from '@/utils/router'
 import {useAuth} from '@/hooks/useAuth.ts'
+import {env} from '@/config'
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -15,7 +15,13 @@ declare module '@tanstack/react-router' {
 }
 
 // Sentry Configuration
-sentryInit()
+// TODO: fix sentry and re-enable
+// existing ERROR:
+// "Refused
+// to connect to 'https://o4506954726703104.ingest.us.sentry.io/api/4506954741055488/envelope/?sentry_key=94ccf213e00340996348fa63f2605456&sentry_version=7&sentry_client=sentry.javascript.react%2F7.108.0'
+// because it violates the following Content Security Policy directive: "default-src 'self'".
+// Note that 'connect-src' was not explicitly set, so 'default-src' is used as a fallback.
+// env.PROD && sentryInit()
 
 // eslint-disable-next-line react-refresh/only-export-components
 const App = () => {
