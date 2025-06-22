12c2292774d8f45cf53cdefa11a3fa9f0c01fa25
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-06-22 22:06:33 +0300

    chore(route): add generated route tree

---
+++ src/routeTree.ts
@@ -0,0 +1,20 @@
+/**
+ routes/
+ ├── (auth)/
+ │   ├── forgot-password.tsx
+ │   ├── login.tsx
+ │   ├── signup.tsx
+ │   └── verify.tsx
+ ├── _forum/
+ │   ├── forum/
+ │       ├── posts/
+ │       │   ├── $postId.tsx
+ │       │   ├── $postId_.edit.tsx
+ │       │   └── create.tsx
+ │       ├── profile/
+ │       │   └── $authorId.tsx
+ │       └── index.tsx
+ ├── __root.tsx
+ ├── _forum.tsx
+ └── index.lazy.tsx
+ */
