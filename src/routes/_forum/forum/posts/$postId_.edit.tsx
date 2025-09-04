6fe559d3d7d4cefda3a81e29e2dee8b5dafdd962
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-09-04 16:06:59 +0300

    refactor routes

--- src/routes/_forum/forum/posts/$postId_.edit.tsx
+++ src/routes/_forum/forum/posts/$postId_.edit.tsx
@@ -4,7 +4,7 @@ import {postLoader} from '@/handlers/loaders/forum.ts'
 import {validateUser} from '@/handlers/loaders/auth.ts'
 import {routeError} from '@/lib/routeError.ts'
 
-export const Route = createFileRoute('/_forum/forum/posts/$postId/edit')({
+export const Route = createFileRoute('/_forum/forum/posts/$postId_/edit')({
     beforeLoad: validateUser,
     loader: async ({params}) => {
         return postLoader((params as {postId: string}).postId) // todo pass post data through route context
