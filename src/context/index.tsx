c541ba9ecd7cbe77dbb2d3f334709ac2b8d21649
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-03-09 19:04:16 +0200

    rfc: add type LayoutProps to context provider

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,20 +1,28 @@
-import {ReactNode} from 'react'
-
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
+import {Toaster} from 'sonner'
+
+import {QueryClientProvider} from '@tanstack/react-query'
+
+import {LayoutProps} from '@/types'
+
+import {queryClient} from '@/lib/queryClient'
 
 import {LangProvider} from '@/context/langContext'
 
 import {persistor, store} from '@/store'
 
-const ContextProvider = ({children}: { children: ReactNode }) => (
+const ContextProvider = ({children}: LayoutProps) => (
     <Provider store={store}>
         <PersistGate
             loading={null}
             persistor={persistor}>
-            <LangProvider>
-                {children}
-            </LangProvider>
+            <QueryClientProvider client={queryClient}>
+                <LangProvider>
+                    {children}
+                </LangProvider>
+                <Toaster/>
+            </QueryClientProvider>
         </PersistGate>
     </Provider>
 )
