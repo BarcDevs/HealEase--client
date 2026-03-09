416dc7a713386c42f1e0fcc3b91f4433c352dc18
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-03-09 18:41:26 +0200

    rfc: add new constants for time periods in ms

---
+++ src/constants/time.ts
@@ -0,0 +1,6 @@
+export const minuteInMs = 60 * 1000
+export const hourInMs = 60 * minuteInMs
+export const dayInMs = 24 * hourInMs
+export const weekInMs = 7 * dayInMs
+export const monthInMs = 30 * dayInMs
+export const yearInMs = 365 * dayInMs
