44b3bd4672b6cec59bf9a9c7c9a690e1cf27974d
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-03-13 16:26:33 +0200

    rfc: update vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    server:{
+        port: 5173,
+        host: true,
+    },
     plugins: [
         react(),
         tailwindcss(),
